3791dbd1bb08480489ca446bed06057ebfee57e8
ryunosuke242 <161914811+ryunosuke242@users.noreply.github.com>
2026-04-14 00:41:05 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
 #define TAP_CODE_DELAY 5
 
