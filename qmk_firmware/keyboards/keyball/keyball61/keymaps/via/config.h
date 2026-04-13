4b43334702465d8694df8bf12159c8d9590e78bc
ryunosuke242 <161914811+ryunosuke242@users.noreply.github.com>
2026-04-13 23:55:42 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -19,7 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
 #ifdef RGBLIGHT_ENABLE
 //#    define RGBLIGHT_EFFECT_BREATHING
 //#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
